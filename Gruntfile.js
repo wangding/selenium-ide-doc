afeb29f056d50ad75c569fb2e13fd21e3c4f6322
wangding <408542507@qq.com>
2020-08-27 15:58:53 +0800

    docs: add cdn

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,25 @@ module.exports = function (grunt) {
         }]
       }
     },
+    replace: {
+      html: {
+        src: ['_book/*.html', '_book/2.9/*.html'],
+        overwrite: true,
+        replacements: [{
+          from: 'href="gitbook',
+          to: 'href="https://cdn.jsdelivr.net/gh/wangding/selenium-ide-doc@gh-pages/gitbook'
+        }, {
+          from: 'href="styles',
+          to: 'href="https://cdn.jsdelivr.net/gh/wangding/selenium-ide-doc@gh-pages/styles'
+        }, {
+          from: 'href="images',
+          to: 'href="https://cdn.jsdelivr.net/gh/wangding/selenium-ide-doc@gh-pages/2.9/images'
+        }, {
+          from: 'src="gitbook',
+          to: 'src="https://cdn.jsdelivr.net/gh/wangding/selenium-ide-doc@gh-pages/gitbook'
+        }]
+      }
+    },
     copy: {
       html: {
         files: [{
@@ -54,6 +73,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-text-replace');
 
-  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify', 'copy:html', 'copy:js']);
+  grunt.registerTask('minify', ['replace', 'htmlmin', 'cssmin', 'uglify', 'copy:html', 'copy:js']);
 };
